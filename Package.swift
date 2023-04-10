b60585a62bf556dc0981d627152fdb936a14b6e2
flujan <lujan.felix80@gmail.com>
2023-04-10 16:38:26 +0200

    added framework

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Swiff",
+    platforms: [
+        .iOS(.v16), .macOS(.v13), .macCatalyst(.v16), .tvOS(.v16), .watchOS(.v9)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +21,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "Swiff",
-            dependencies: []),
-        .testTarget(
-            name: "SwiffTests",
-            dependencies: ["Swiff"]),
+            path: "./Sources/Swiff.xcframework"
+        )
     ]
 )
---
+++ Sources/Swiff.xcframework/tvos-arm64/Swiff.framework/Headers/Swiff.h
@@ -0,0 +1,18 @@
+//
+//  Swiff.h
+//  Swiff
+//
+//  Created by flujan on 10/3/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swiff.
+FOUNDATION_EXPORT double SwiffVersionNumber;
+
+//! Project version string for Swiff.
+FOUNDATION_EXPORT const unsigned char SwiffVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiff/PublicHeader.h>
+
+
